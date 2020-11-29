d852d631c44e3f8f7a982d70b44f03ee297858c9
Daniel <dfsantosbu@unal.edu.co>
2020-11-29 18:12:07 +0100

    Add readme.

---
+++ readme.md
@@ -0,0 +1,1 @@
+This is a small lexical analyser for Rust, it was done in a very short time, and as task of the university, then no expect too much. Feel free to contribute there are lot of stuff to change.
--- src/core.hpp
+++ src/core.hpp
@@ -1,28 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-const vector<std::string> keywords{"mut", "let", "in", "fn", "std", "struct", "println!", "use", "return"};
-const vector<std::string> statements{"for", "while", "if", "else"};
-const vector<std::string> operators{"*", "<" ,">", "+", "-", "%", "/", "=", "-=", "*=", "+=", "/=", "++", "--", "==", "&", "|", "^"};
+const vector<string> keywords{"mut", "let", "in", "fn", "std", "struct", "println!", "use", "return"};
+const vector<string> statements{"for", "while", "if", "else"};
+const vector<string> operators{"*", "<" ,">", "+", "-", "%", "/", "=", "-=", "*=", "+=", "/=", "++", "--", "==", "&", "|", "^"};
 
-bool isKeyWord(const std::string &str){
+bool isKeyWord(const string &str){
   return ( find(keywords.begin(), keywords.end(), str) != keywords.end() )||
-          ( find(statements.begin(), statements.end(), str) != statements.end());
+    ( find(statements.begin(), statements.end(), str) != statements.end());
 }
 
-bool isOperator(const std::string &str){
-  return  std::find(operators.begin(), operators.end(), str) != operators.end();
+bool isOperator(const string &str){
+  return  find(operators.begin(), operators.end(), str) != operators.end();
 }
 
-bool isString(const std::string &str){
+bool isString(const string &str){
   return str.size()>1&& str[0] == '"' && str[str.size()-1] == '"';
 }
 
-bool isAlphaNum(const std::string &str){
-  return str.size()>0 && std::all_of(str.begin(), str.end(), ::isalnum);
+bool isAlphaNum(const string &str){
+  return str.size()>0 && all_of(str.begin(), str.end(), ::isalnum);
 }
 
-bool isNumber( const std::string &str){
-  return str.size()>0 && std::all_of(str.begin(), str.end(), ::isdigit);
+bool isNumber( const string &str){
+  return str.size()>0 && all_of(str.begin(), str.end(), ::isdigit);
 }
 
