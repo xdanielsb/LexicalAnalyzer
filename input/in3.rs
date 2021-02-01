bdbc2afeb128eb63ba1e1c281be1cfb40505fb6b
Daniel <dfsantosbu@unal.edu.co>
2021-02-01 19:57:06 +0100

    chore: trace errors.

--- input/in3.rs
+++ input/in3.rs
@@ -3,7 +3,7 @@
  */
 fn main(){
     // this is the value
-    let mut x  = 3;
+    let mut x = 3;
     if x % 2 == 0 { 
         println!("this is an even number");
     }
--- main.cpp
+++ main.cpp
@@ -25,43 +25,74 @@ bool isCommentBlock = false;
 bool openedQuote = 0;
 
 
-enum{iskeyword, isoperator, isconstant,isnew,isstring,isblock, isnumbers, isident };
+enum{iskeyword, isoperator, isconstant,isstring,isblock, isnumbers, isident };
 int ln = 1, last_id = -1;
-bool check( const string& buf){
+vector < string > tokens;
+map< string, int> symbols;
+
+
+void error(const string &err){
+   cerr << err <<endl;
+   cout << "\t line:" << ln << endl; 
+   exit(0);
+}
+
+bool check( string& buf){
+  if( buf.size() && buf.back() == '\n') buf.erase(buf.end());
   if(isKeyWord(buf)){
-    cout<< " \n----LINE #"<< ln<< "  keyword: "<< buf << endl;
+    cout<< "----LINE #"<< ln<< "  keyword: "<< buf << endl;
     last_id = iskeyword;
+    symbols[buf] = iskeyword;
     return true;
   }
-  if((last_id == iskeyword || last_id == isconstant || last_id==isnumbers || last_id ==isident) && isOperator(buf)){
+  if(isOperator(buf)){
+    //(last_id == iskeyword || last_id == isconstant || last_id==isnumbers || last_id ==isident) &&
+    if(last_id == iskeyword)error("E01: you cannot have a keyword followed by an operator");
+    if(last_id == -1)error("E06: you need to have a number or identifier before an operator");
     last_id = isoperator;
-    cout<< " \n----LINE #"<< ln<< "  operator: "<< buf << endl;
+    cout<< "----LINE #"<< ln<< "  operator: "<< buf << endl;
+    symbols[buf] = isoperator;
     return true;
   }
   if(isString(buf)){
+    if(last_id == isstring)error("E02: you cannot have a string followed by a string");
+    if(last_id == iskeyword)error("E03: you cannot have a string followed by a keyword");
+    if(last_id == isnumbers)error("E04: you cannot have a string followed by a number");
+    if(last_id == isident)error("E05: you cannot have a string followed by an identifier");
     last_id = isstring;
-    cout<< " \n----LINE #"<< ln<< "  string: "<< buf << endl;
+    cout<< "----LINE #"<< ln<< "  string: "<< buf << endl;
+    symbols[buf] = isstring;    
     return true;
   }
   if(isNumber(buf)){
+    if(last_id == isstring)error("E07: you cannot have a number followed by a string");
+    if(last_id == iskeyword)error("E08: you cannot have a number followed by a keyword");
+    if(last_id == isnumbers)error("E09: you cannot have a number followed by a number");
+    if(last_id == isident)error("E010: you cannot have a number followed by an identifier");
     last_id = isnumbers;
-    cout<< " \n----LINE #"<< ln<< "  number: "<< buf << endl;
+    symbols[buf] = isnumbers;    
+    cout<< "----LINE #"<< ln<< "  number: "<< buf << endl;
     return true;
   }
   else if(isAlphaNum(buf)){
+    if(last_id == isident)error("E011: you cannot have an identifier followed by an identifier");
+    if(last_id == isstring)error("E12: you cannot have an id followed by a string");
+    if(last_id == isnumbers)error("E14: you cannot have an id followed by a number");
     last_id = isident;
-    cout<< " \n----LINE #"<< ln<< "  identifier: "<< buf << endl;
+    symbols[buf] = isident;    
+    cout<< "----LINE #"<< ln<< "  identifier: "<< buf << endl;
     return true;
   }
   else{
     if(buf.size()>0){
       // Not clasified yet
-      cout<< " \n----LINE #"<< ln<< "  not classified "<< buf << endl;
+      cout<< "----LINE #"<< ln<< "  not classified "<< buf << endl;
 
     }
   }
   return false;
 }
+
 
 int main( int argc, char** argv ){
   ios::sync_with_stdio( false );
@@ -72,13 +103,13 @@ int main( int argc, char** argv ){
   const string &pathfile=argc<=1?"input/in1.rs":argv[1];
   fstream file(pathfile, std::fstream::in);
 
-  if (!file.is_open()){
-    cout << "Cannot open the file\n";
-    exit(0);
-  }
+  if (!file.is_open())
+    error("E15: cannot open the file");
 
   while (file >> std::noskipws >> ch){
-    if( ch == '\n') ln++;
+    if( ch == '\n') {
+      ln++;
+    }
     if(openedQuote|| ch == '"'){
       buf+=ch;
       if(!openedQuote){
@@ -93,11 +124,11 @@ int main( int argc, char** argv ){
     if( isCommentLine || isCommentBlock){
       if(isCommentLine && ch =='\n'){
         isCommentLine = false;
-        cout<< " \n----LINE #"<< ln<< "  line-comment: "<< buf << endl;
+        cout<< "----LINE #"<< ln<< "  line-comment: "<< buf << endl;
         buf="";
       }else if(isCommentBlock && buf.lst == '*' && ch == '/'){
         isCommentBlock = false;
-        cout<< " \n----LINE #"<< ln<< "  block-comment: "<< buf << "/" <<endl;
+        cout<< "----LINE #"<< ln<< "  block-comment:\n"<< buf << "/" <<endl;
         buf="";
       }else{
         buf+=ch;
@@ -112,30 +143,41 @@ int main( int argc, char** argv ){
       }
     }
     if( ch == '{' || ch == '('){
+      tokens.push_back(string(1,ch));
       brack.push(ch=='{'?'}':')');
-      check(buf);
+      cout<< "----LINE #"<< ln<< "  scope: "<< ch << endl;
       last_id= isblock;
       buf="";
     }else if ( ch == '}' || ch == ')'){
       if(brack.size() == 0|| brack.top()!= ch) {
-        cout << "Invalid region at Line #" << ln <<endl;
-        exit(0);
+          error("E17: Malformed brackets");
       }
+      tokens.push_back(string(1,ch));
+      cout<< "----LINE #"<< ln<< "  scope: "<< ch << endl;
       buf="";
       brack.pop();
     }else if (ch == ';'){
-      check(buf);
-      last_id = isnew;
+      if(check(buf))
+        tokens.push_back(buf);
+      last_id =-1;
       buf="";
     }else{
       if(isspace(ch) || ch=='\n'){
-        if(buf.size()>0)
-          check(buf);
+        if(buf.size()>0 && check(buf))
+          tokens.push_back(buf);
         buf="";
       }else{
         buf+=ch;
       }
     }
   }
+  if( brack.size()) error("E16: Malformed brackets");
+  if( isCommentBlock) error("E17: Malformed comments");
+    
+  //tokens
+  /* for( string s: tokens) d0(s); */
+
+  //symbols
+  /* for ( pair<string,int> f: symbols) d0(f.first); */
   return 0;
 }
